3274b39c718e78cc569fe8a0aeefef9e71c017ea
sota-mitsumori <sota.mitsumori@gmail.com>
2025-01-11 21:45:09 +0900

    16 bit index

--- enc.c
+++ enc.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include "grpwk24.h"
 
-#define BITS_per_block 50 // 25文字ブロックあたりのビット数(2bit * 25)
-// #define NUM_BLOCKS 5000  // 25文字ブロックの数(2*10^5/50)
-// #define CHUNK_SIZE 4 //処理するバイト数
 
 int enc(){
   FILE *ofp;
@@ -19,33 +16,20 @@ int enc(){
     exit(1);
   }
 
-  unsigned long long block_index = 0; // 25文字ブロックのインデックス
-  unsigned char c1, c2, c3, c4, res;
-  unsigned char bit_array[BITS_per_block] = {0}; //50bit分格納のための配列, 0で初期化
+  unsigned char res;
 
-  for(int i=0; i<ORGDATA_LEN; i+=4){ // ORGDATA_LEN = 2 * 10 ^ 5で、頭から4bit取り出す
-    c1 = getc(ofp);
-    c2 = getc(ofp);
-    c3 = getc(ofp);
-    c4 = getc(ofp);
-
-    // c1,c2,c3,c4の最下位ビットを配列に格納（最下位ビット以外は全て0）
-    bit_array[0] = c1 & 0x1;
-    bit_array[1] = c2 & 0x1;
-    bit_array[2] = c3 & 0x1;
-    bit_array[3] = c4 & 0x1;
-
-    // 残りの46(50-4)ビット分の配列にインデックスを格納（インデックスの上位ビットから1ビットずつ）
-    for (int j = 4; j < BITS_per_block; j++) {
-        // 上位ビットから取得
-        bit_array[j] = (block_index >> (BITS_per_block - 1 - j)) & 0x1;  
+  for(int i=0; i<ORGDATA_LEN; i+=12){
+    
+    for (int j=0;j<2;j++) {
+      fputc(BASE_A, efp); // 文字数稼ぎ
+    }
+    unsigned char  str[46] = {0};
+    for (int j = 0; j < 16; j++) {
+          str[15 - j] = (i >> j) & 1; // jビット目を取得しリストに格納（MSBがリストの最初になるように逆順）
     }
 
-    //2bitずつ、DNA塩基に変化して出力
-    for (int j = 0; j < BITS_per_block; j += 2) {
-      //2bitの値を計算
-      unsigned char two_bits = (bit_array[j] << 1) | bit_array[j+1];
-      switch(two_bits){
+    for (int j = 0; j < 16; j+=2) {
+          switch( ( (str[j] & 0x1) << 7) >> 6 | ( str[j+1] & 0x1) ){
         case 0:
           res = BASE_A;
           break;
@@ -58,13 +42,50 @@ int enc(){
         case 3:
           res = BASE_T;      
           break;
-      }
-      fputc(res, efp);
+        }
+        fputc(res, efp);
     }
-  
-    block_index ++;
 
-  } 
+    
+    for (int j = 16; j < 37; j+=7) {
+      str[j] = getc(ofp);
+      str[j+1] = getc(ofp);
+      str[j+2] = getc(ofp);
+      str[j+3] = getc(ofp);
+      for (int k = j; k < j+4; k+=2) {
+          switch( ( (str[k] & 0x1) << 7) >> 6 | ( str[k+1] & 0x1) ){
+        case 0:
+          res = BASE_A;
+          break;
+        case 1:
+          res = BASE_C;      
+          break;
+        case 2:
+          res = BASE_G;      
+          break;
+        case 3:
+          res = BASE_T;      
+          break;
+        }
+        fputc(res, efp);
+    }
+
+      str[j+4] = (str[j] & 1) ^ (str[j+1] & 1) ^ (str[j+3] & 1);
+      str[j+5] = (str[j] & 1) ^ (str[j+2] & 1) ^ (str[j+3] & 1);
+      str[j+6] = (str[j+1] & 1) ^ (str[j+2] & 1) ^ (str[j+3] & 1);
+
+      for (int k = j+4; k < j+7; k++) {
+        if (str[k] == 1) {
+          res = BASE_C;
+        } else {
+          res = BASE_G;
+        }
+        fputc(res, efp);
+      }
+    }
+
+  }
+    
   res = '\n';
   fputc(res, efp);
   
